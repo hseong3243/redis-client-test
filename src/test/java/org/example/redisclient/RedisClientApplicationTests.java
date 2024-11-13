445ef034936788e8267a5c560f125df072b8b785
hseong3243 <hseong3243@gmail.com>
2024-11-13 17:07:06 +0900

    제디스 빈 등록 여부 확인

--- src/test/java/org/example/redisclient/RedisClientApplicationTests.java
+++ src/test/java/org/example/redisclient/RedisClientApplicationTests.java
@@ -1,9 +1,15 @@
 package org.example.redisclient;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class RedisClientApplicationTests {
@@ -11,10 +17,16 @@ class RedisClientApplicationTests {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
         for (String bean : applicationContext.getBeanDefinitionNames()) {
             System.out.println("bean: " + bean);
         }
+        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
+        assertThat(connectionFactory).isInstanceOf(LettuceConnectionFactory.class);
+        assertThat(connectionFactory).isNotInstanceOf(JedisConnectionFactory.class);
     }
 }
